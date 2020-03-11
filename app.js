6372b960f8033ee09b67be891ad25414f6889de4
siimerxd <risto.siimer@ametikool.ee>
2020-03-11 13:10:41 +0200

    2

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ let occupations = {
     7512:{name:'Pagar', rate: 0}
 }
 
+const occupationSelect = document.getElementById('occupation-select')
+
+for ( const key in occupations ) {
+    console.log(occupations[key].name)
+}
+
 fetch('http://andmebaas.stat.ee/sdmx-json/data/PA633/DBL245+DBL315+DBL330+DBL343+DBL349.3.1/all?startTime=2014&endTime=2014&dimensionAtObservation=allDimensions')
     .then(Response => {
         return Response.json()
@@ -12,10 +18,15 @@ fetch('http://andmebaas.stat.ee/sdmx-json/data/PA633/DBL245+DBL315+DBL330+DBL343
     .then(data => {
        console.log(data)
 
-       data.structure.dimensions.observation[0].values.forEach(element => {
+       data.structure.dimensions.observation[0].values.forEach((element,index) => {
+           const occupationkey = (element.name.split(' ')[0])
+           const datasetkey = index + ':0:0:0'
            console.log(element.name.split(' ')[0])
-       });
+            console.log(datasetkey)
+            console.log.(data.datasets[0].observations[dataSetKey][0])
+            occupations[occupationkey].rate = data.datasets[0].observations[dataSetKey][0]
+        });
 
-       
+       console.log(occupations)
     })
     
